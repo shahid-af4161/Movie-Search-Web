f8a7c80ee47ae364b49cb7c14a0961e10172ed34
shahid <sa4656683@gmail.com>
2026-06-10 14:32:45 +0530

    feat: added Multiple search results, Refactored createMovieCard()

--- app.js
+++ app.js
@@ -11,39 +11,59 @@ const movieContainer = document.querySelector("#movie-cards");
 // };
 
 async function searchMovie() {
+    movieContainer.innerHTML = "";
     console.log("btn clicked");
     if (input.value.trim() === "") {
         alert("Enter movie name");
+        return;
     } else {
         console.log(`Searching for: ${input.value}`);
     }
 
-    let movie = await getMovie(input.value);
-    if (movie.Response === 'False') {
-        movieContainer.innerHTML = `<h3>${movie.Error}</h3>`;
-        console.log(movie);
+    let movieFetch = await getMovie(input.value);
+    console.log(movieFetch);
+    if (movieFetch.Response === 'False') {
+        movieContainer.innerHTML = `<h3>${movieFetch.Error}</h3>`;
+        // console.log(movieFetch);
         return;
     }
+     
+    let movieList = movieFetch.Search;
+    console.log(movieList);
+    movieList.forEach(movie => {
+        // console.log(movie);
+        createMovieCard(movie);
+    });
 
+}
+function createMovieCard(movie) {
     let movieCard = document.createElement("div");
     let poster = document.createElement("img");
     let title = document.createElement("h3");
     let releaseYear = document.createElement("p");
+    let type = document.createElement("p");
     let rating = document.createElement("p");
 
 
     poster.src = movie.Poster;
     title.innerText = movie.Title;
     releaseYear.innerText = movie.Year;
-    rating.innerText = `IMDb : ${movie.imdbRating}`;
+    type.innerText = `Type : ${movie.Type}`;
+    rating.innerText = `IMDb : ${movie.imdbID}`;
     movieCard.setAttribute("class", "movie-card");
 
     movieContainer.appendChild(movieCard);
     movieCard.appendChild(poster);
     movieCard.appendChild(title);
     movieCard.appendChild(releaseYear);
-    movieCard.appendChild(rating);
+    movieCard.appendChild(type);
+    // movieCard.appendChild(rating);
+
+    movieCard.addEventListener("click",()=>{
+    console.log(movie.imdbID);
+    });
 }
+
 btn.addEventListener("click", searchMovie);
 
 input.addEventListener("keydown", (event) => {
@@ -51,18 +71,24 @@ input.addEventListener("keydown", (event) => {
     if (event.key == 'Enter') {
         searchMovie();
     }
-})
+});
+
+
+
 
 
 
 async function getMovie(movieName) {
-    let url = `https://www.omdbapi.com/?apikey=4c3cd847&t=${movieName}`;
+    let url = `https://www.omdbapi.com/?apikey=4c3cd847&s=${movieName}`;
     try {
         let response = await axios.get(url);
-        console.log(response.data);
+        console.log(response.data.Search); // returns an array
+        // response.data.Search.forEach(movie =>{
+        //     console.log(movie);
+        // })
         return response.data;
+
     } catch (e) {
         console.log("Invalid Request ", e);
     }
-
 }
